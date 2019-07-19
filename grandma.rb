53733110fa6b89eb65bf81f1b0b91a86790b3d26
Toron Seigler <toronseigler9@gmail.com>
2019-07-19 21:45:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-
+if phrase == " Hi Nana, how are you?"
+  return "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
