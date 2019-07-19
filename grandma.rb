d885d596e3c73ee61dae2ea09ef40c5d8d7333f0
Toron Seigler <toronseigler9@gmail.com>
2019-07-19 21:56:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase == "Hi Nana, how are you?" || phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
